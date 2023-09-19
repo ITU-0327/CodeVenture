398e05c215814407d1581eb79e751e3395cc9316
matthewchungkaishing <mchu0055@student.monash.edu>
2023-09-20 06:27:21 +1000

    register VideoTutorial in admin page

--- LearningResource/admin.py
+++ LearningResource/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
-from .models import SubModule, LearningModule
+from .models import SubModule, LearningModule, VideoTutorial
 
 admin.site.register(SubModule)
 admin.site.register(LearningModule)
+admin.site.register(VideoTutorial)
