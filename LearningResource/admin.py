636324ad18595d2ff16f5dba53dc265b8b09742a
matthewchungkaishing <mchu0055@student.monash.edu>
2023-09-20 03:13:24 +1000

    Register SubModule and LearningModule to admin page

--- LearningResource/admin.py
+++ LearningResource/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import SubModule, LearningModule
+
+admin.site.register(SubModule)
+admin.site.register(LearningModule)
