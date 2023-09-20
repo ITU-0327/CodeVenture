0f121f7ff27267337a5599d10060daa0823532de
matthewchungkaishing <mchu0055@student.monash.edu>
2023-09-21 03:25:15 +1000

    Add login and logout page

--- UserManagement/views.py
+++ UserManagement/views.py
@@ -1,3 +1,43 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.models import User
 
-# Create your views here.
+
+def loginPage(request):
+    if request.user.is_authenticated:
+        return redirect('home')
+
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        if not username or not password:
+            messages.error(request, 'Both username and password must be provided.')
+            return render(request, 'login_register.html')
+
+        try:
+            User.objects.get(username=username)
+        except User.DoesNotExist:
+            messages.error(request, 'User does not exist')
+            return render(request, 'login_register.html')
+
+        user = authenticate(request, username=username, password=password)
+        if user:
+            login(request, user)
+            return redirect('home')
+        else:
+            messages.error(request, 'Username or Password wrong.')
+    context = {
+
+    }
+    return render(request, 'login_register.html', context)
+
+
+def logoutUser(request):
+    storage = messages.get_messages(request)
+    for _ in storage:
+        pass
+    storage.used = True
+    logout(request)
+    return redirect('home')
