e54412796ad896516d66c0f33da1f13bbaf0d884
matthewchungkaishing <mchu0055@student.monash.edu>
2023-09-21 21:15:04 +1000

    add the html for the welcome page

--- WelcomePage/views.py
+++ WelcomePage/views.py
@@ -2,4 +2,5 @@ from django.shortcuts import render
 
 
 def home_view(request):
-    return render(request, 'base_generic.html')
+    # return render(request, 'base_generic.html')
+    return render(request, 'index.html')
