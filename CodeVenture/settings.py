4f5388a51522604fcca6dfe52fdf9d00d2fad319
matthewchungkaishing <mchu0055@student.monash.edu>
2023-10-29 07:09:04 +1100

    Add Database connections

--- CodeVenture/settings.py
+++ CodeVenture/settings.py
@@ -104,10 +104,23 @@ WSGI_APPLICATION = 'CodeVenture.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'codeventure-db',
+        'USER': 'root',
+        'PASSWORD': '7(YPQRqCHg]<V%zV',
+        'HOST': '34.129.128.151',
+        'PORT': '3306',
+        'OPTIONS': {
+            'charset': 'utf8mb4',
+        },
     }
 }
 
